5903480b8e09520839b6ce4facca21cd5b1c7dcb
PradeepKundekar0101 <pradeepkundekargithub@gmail.com>
2026-02-14 11:28:21 +0530

    Enhance Facebook Pixel integration by adding TypeScript ESLint comments to suppress explicit any type warnings in pixel tracking functions.

--- src/lib/fpixel.ts
+++ src/lib/fpixel.ts
@@ -2,13 +2,14 @@ export const FB_PIXEL_ID = process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID;
 
 export const pageview = () => {
   if (typeof window !== "undefined") {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (window as any).fbq("track", "PageView");
   }
 };
 
-// https://developers.facebook.com/docs/facebook-pixel/advanced/
 export const event = (name: string, options = {}) => {
   if (typeof window !== "undefined") {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     (window as any).fbq("track", name, options);
   }
 };
